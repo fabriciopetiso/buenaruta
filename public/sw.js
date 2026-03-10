9760dffe1ff23ca43a5ca036d81d5dc56f636c3f
Fabricio <fabriciopuzio@altermundi.net>
2026-03-10 10:02:05 -0300

    Disable SW completely - fix loading issues

--- public/sw.js
+++ public/sw.js
@@ -1,42 +1,6 @@
-const CACHE_NAME = 'buenaruta-v3';
-const STATIC_ASSETS = [
-  '/icon-192.png',
-  '/icon-512.png',
-  '/buena-ruta.mp3'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => 
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  // Solo iconos y audio van al cache
-  const isStatic = STATIC_ASSETS.some(asset => url.pathname === asset);
-  
-  if (isStatic) {
-    event.respondWith(
-      caches.match(event.request).then((cached) => cached || fetch(event.request))
-    );
-    return;
-  }
-
-  // TODO LO DEMÁS: red directa, sin cache, nunca
-  event.respondWith(fetch(event.request));
+// SW desactivado - se auto-desregistra
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.registration.unregister();
+  self.clients.matchAll().then(clients => clients.forEach(c => c.navigate(c.url)));
 });
